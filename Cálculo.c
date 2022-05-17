5e42a0b8c53d3bd0b2d602b4e9859ab374e7786f
João Lucas <Jlucasoc@hotmail.com>
2022-05-17 15:01:29 -0300

    "Cálculo" is done ✅

---
+++ Cálculo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+
+void ler(double x, int i, double soma, double n, double d)
+{
+    if(i > 1){
+        if(i % 2 == 0){
+            soma += pow(2, i-1) / d;
+
+            d += 3;
+        }
+        if(i % 2 != 0){
+            soma += n / pow(2, i-1);
+            n += 2;
+        }
+    }
+
+    if(i == 1){
+        soma++;
+    }
+    if(i >= x){
+        printf("S: %.2lf\n", soma);
+        return;
+    }
+    return ler(x, i+1, soma, n, d);
+}
+
+
+int main()
+{
+    double x;
+    scanf("%lf", &x);
+
+    ler(x, 1, 0, 3, 3);
+}
